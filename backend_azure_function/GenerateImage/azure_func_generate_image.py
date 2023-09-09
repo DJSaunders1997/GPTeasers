efab860513f2949e2d358a10742b6b77c244e902
Dave <DJSaunders1997@gmail.com>
2023-09-09 12:59:12 +0100

    Add azure example code

---
+++ backend_azure_function/GenerateImage/azure_func_generate_image.py
@@ -0,0 +1,32 @@
+import logging
+from azure.functions import HttpRequest, HttpResponse
+from generate_image import generate_image
+
+def main(req: HttpRequest) -> HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    prompt = req.params.get('prompt')
+    if not prompt:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            prompt = req_body.get('prompt')
+
+    if prompt:
+        try:
+            # generate_image() function returns the URL of the generated image
+            image_url = generate_image(prompt)
+
+            # Return the image URL in the HTTP response
+            return HttpResponse(f"Generated Image URL: {image_url}", status_code=200)
+
+        except Exception as e:
+            # Handle exceptions that might occur during image generation
+            return HttpResponse(f"Error generating image: {str(e)}", status_code=500)
+    else:
+        return HttpResponse(
+            "Please provide a prompt in the query string or in the request body to generate an image.",
+            status_code=400
+        )
---
+++ backend_azure_function/GenerateQuiz/auzre_func_generate_quiz.py
@@ -0,0 +1,32 @@
+import logging
+from azure.functions import HttpRequest, HttpResponse
+from generate_quiz import generate_quiz
+
+def main(req: HttpRequest) -> HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    topic = req.params.get('topic')
+    if not topic:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            topic = req_body.get('topic')
+
+    if topic:
+        try:
+            # The generate_quiz() function returns the quiz based on the provided topic
+            quiz = generate_quiz(topic)
+
+            # Return the generated quiz in the HTTP response
+            return HttpResponse(f"Generated Quiz: {quiz}", status_code=200)
+
+        except Exception as e:
+            # Handle exceptions that might occur during quiz generation
+            return HttpResponse(f"Error generating quiz: {str(e)}", status_code=500)
+    else:
+        return HttpResponse(
+            "Please provide a topic in the query string or in the request body to generate a quiz.",
+            status_code=400
+        )
