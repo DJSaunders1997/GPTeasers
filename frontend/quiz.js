6138cfefe6af6b4ade42b8b7f984ef00c98bbb71
Dave <DJSaunders1997@gmail.com>
2023-09-13 00:18:29 +0100

    Display results in buttons

--- frontend/quiz.js
+++ frontend/quiz.js
@@ -1,54 +1,79 @@
-// simple class called Quiz that wraps around the JSON response quz data 
+// simple class called Quiz that wraps around the JSON response quz data
 
 class Quiz {
-    // Example data:
-    // [
-    //     {
-    //         "question_id": 1,
-    //         "question": "What is the scientific name of domestic cats?",
-    //         "A": "Felis catus",
-    //         "B": "Canis lupus",
-    //         "C": "Panthera leo",
-    //         "answer": "A",
-    //         "explanation": "The scientific name of domestic cats is Felis catus.",
-    //         "wikipedia": "https://en.wikipedia.org/wiki/Domestic_cat",
-    //         "dalle_prompt": "Image of a domestic cat"
-    //     },
-    //     {
-    //         "question_id": 2,
-    //         "question": "Which breed of cat is known for its short legs?",
-    //         "A": "Persian",
-    //         "B": "Siamese",
-    //         "C": "Munchkin",
-    //         "answer": "C",
-    //         "explanation": "The breed of cat known for its short legs is the Munchkin.",
-    //         "wikipedia": "https://en.wikipedia.org/wiki/Munchkin_cat",
-    //         "dalle_prompt": "Image of a Munchkin cat"
-    //     }
-    // ]
+  // Example data:
+  // [
+  //     {
+  //         "question_id": 1,
+  //         "question": "What is the scientific name of domestic cats?",
+  //         "A": "Felis catus",
+  //         "B": "Canis lupus",
+  //         "C": "Panthera leo",
+  //         "answer": "A",
+  //         "explanation": "The scientific name of domestic cats is Felis catus.",
+  //         "wikipedia": "https://en.wikipedia.org/wiki/Domestic_cat",
+  //         "dalle_prompt": "Image of a domestic cat"
+  //     },
+  //     {
+  //         "question_id": 2,
+  //         "question": "Which breed of cat is known for its short legs?",
+  //         "A": "Persian",
+  //         "B": "Siamese",
+  //         "C": "Munchkin",
+  //         "answer": "C",
+  //         "explanation": "The breed of cat known for its short legs is the Munchkin.",
+  //         "wikipedia": "https://en.wikipedia.org/wiki/Munchkin_cat",
+  //         "dalle_prompt": "Image of a Munchkin cat"
+  //     }
+  // ]
   constructor(data) {
-      this.questions = data;
-      this.currentIndex = 0;
+    this.questions = data;
+    this.currentIndex = 0;
   }
 
   // Get the current question without moving to the next one
   getCurrentQuestion() {
-      if (this.currentIndex < this.questions.length) {
-          return this.questions[this.currentIndex];
-      } else {
-          return null;
-      }
+    if (this.currentIndex < this.questions.length) {
+      return this.questions[this.currentIndex];
+    } else {
+      alert("Quiz over!");
+      return null;
+    }
   }
 
   // Move to the next question and return it
   nextQuestion() {
-      this.currentIndex++;
-      return this.getCurrentQuestion();
+    this.currentIndex++;
+    return this.getCurrentQuestion();
   }
 
   // Check if there are more questions in the quiz
   hasMoreQuestions() {
-      return this.currentIndex < this.questions.length - 1;
+    return this.currentIndex < this.questions.length - 1;
+  }
+
+  checkAnswer(selectedOption) {
+    this.quiz.getCurrentQuestion();
+
+    if (selectedOption === currentQuestion.answer) {
+      alert("Correct!");
+    } else {
+      //TODO: Eventuall move this UI to UI class
+      alert(
+        "Wrong! The correct answer is: " +
+          currentQuestion.answer +
+          ".\nExplanation: " +
+          currentQuestion.explanation +
+          ".\nWikipedia: " +
+          currentQuestion.wikipedia
+      );
+    }
+    if (this.hasMoreQuestions()) {
+      this.nextQuestion();
+      ths.displayCurrentQuestion();
+    } else {
+      alert("Quiz Finished!");
+    }
   }
 }
 
