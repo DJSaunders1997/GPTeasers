ee6c0320e2778d94eca1f5dec4b2ce075209a5a5
Dave <DJSaunders1997@gmail.com>
2023-09-17 12:16:20 +0100

    Increase loading bar to 60s

--- frontend/loadingbar.js
+++ frontend/loadingbar.js
@@ -10,9 +10,9 @@ class LoadingBar {
 
   /**
    * Start the loading bar animation.
-   * @param {number} [duration=25000] - Duration in milliseconds for the loading bar to complete.
+   * @param {number} [duration=60000] - Duration in milliseconds for the loading bar to complete.
    */
-  start(duration = 25000) {
+  start(duration = 60000) {
     // Reset the bar
     this.loadingBar.style.width = "0%";
 
@@ -27,7 +27,7 @@ class LoadingBar {
         width++;
         this.loadingBar.style.width = width + "%";
       }
-    }, duration / 100); // e.g., for 25000ms (25s), it will increase width every 250ms.
+    }, duration / 100); // e.g., for 60000ms (60s), it will increase width every 600ms.
   }
 
   /**
